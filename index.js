3ba7389cacc4a1c2a917cb24ad570beda18e9847
Gugaligu <sharanov-2014@mail.ru>
2024-08-30 17:41:21 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let tel = document.getElementById("tel")
+let tel = document.getElementById("tel");
 let html = ``;
-let copy=document.getElementById("copy")
-let del=document.getElementById("del")
+let copy=document.getElementById("copy");
+let del=document.getElementById("del");
+let send=document.getElementById("send");
+let tg=window.Telegram.WebApp;
+tg.expand()
 
-
-
-
-let lock = document.getElementById("lock")
+let lock = document.getElementById("lock");
 lock.onclick = function () {
     let namepara = document.getElementById("namepara").value;
     let numberpara = document.getElementById("numberpara").value;
@@ -16,22 +16,26 @@ lock.onclick = function () {
     document.getElementById("numberpara").value = '';
     document.getElementById("daypara").value = '';
     document.getElementById("numbernedel").value = '';
-    nedel= nedel.split(",").join("+")
-    nedel= nedel.split(" ").join("")
+    nedel= nedel.split(",").join("+");
+    nedel= nedel.split(" ").join("");
     tel.innerHTML=tel.innerHTML+`<br>[${namepara},${numberpara},${daypara},${nedel}]`
-    console.log(namepara)
-    console.log(numberpara)
-    console.log(daypara)
-    console.log(nedel)
+    console.log(namepara);
+    console.log(numberpara);
+    console.log(daypara);
+    console.log(nedel);
 }
 
 copy.onclick=function(){
     navigator.clipboard.writeText(tel.innerText);
-    console.log(tel.innerText)
+    console.log(tel.innerText);
 }
 del.onclick=function(){
-    tel.innerHTML=``
-    console.log("удалил")
+    tel.innerHTML=``;
+    console.log("удалил");
+}
+send.onclick=function(){
+    tg.sendData(tel.innerText)
+    tg.close()
 }
 
 
