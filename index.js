f9bca17b641f1d0fab9d8ae6ab7ce075a8a5e0b3
Gugaligu <sharanov-2014@mail.ru>
2024-09-21 18:20:40 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,29 +18,29 @@ window.onload = lod;
 console.log(localStorage.getItem(`light`))
 col_button.innerHTML = color_back
 function lod() {
-    if (col_button.innerHTML.includes("/sun")) {
+    if (col_button.innerHTML.includes("sun")) {
         console.log("солнышко")
-        col_button.innerHTML = `<img src="/sun.png" style="height: 3vh;">`
+        col_button.innerHTML = `<img src="sun.png" style="height: 3vh;">`
         document.querySelector(`.color_nastr`).style.cssText = `background: rgb(199, 199, 199);border: #fff1d7;`
     }
-    if (col_button.innerHTML.includes("/moon")) {
+    if (col_button.innerHTML.includes("moon")) {
         console.log("луна")
-        col_button.innerHTML = `<img src="/moon.png" style="height: 3vh;">`
+        col_button.innerHTML = `<img src="moon.png" style="height: 3vh;">`
         document.querySelector(`.color_nastr`).style.cssText = `background-color: #313131;border: #fff1d7;`
     }
     if (localStorage.getItem(`light`) === null) {
-        localStorage.setItem(`light`, `<img src="/sun.png" style="height: 3vh;">`)
+        localStorage.setItem(`light`, `<img src="sun.png" style="height: 3vh;">`)
     }
 }
 col_button.onclick = function () {
     let col_button = document.getElementById("col")
-    if (col_button.innerHTML.includes("/sun")) {
-        localStorage.setItem(`light`, `<img src="/moon.png" style="height: 3vh;">`)
-        col_button.innerHTML = `<img src="/moon.png" style="height: 3vh;">`
+    if (col_button.innerHTML.includes("sun")) {
+        localStorage.setItem(`light`, `<img src="moon.png" style="height: 3vh;">`)
+        col_button.innerHTML = `<img src="moon.png" style="height: 3vh;">`
         document.querySelector(`.color_nastr`).style.cssText = `background: #313131;border: #fff1d7;`
     } else {
-        localStorage.setItem(`light`, `<img src="/sun.png" style="height: 3vh;">`)
-        col_button.innerHTML = `<img src="/sun.png" style="height: 3vh;">`
+        localStorage.setItem(`light`, `<img src="sun.png" style="height: 3vh;">`)
+        col_button.innerHTML = `<img src="sun.png" style="height: 3vh;">`
         document.querySelector(`.color_nastr`).style.cssText = `background-color: rgb(199, 199, 199);border: #fff1d7;`
     }
     console.log(localStorage.getItem("light"))
